b91f6754763887eb6efd382dd3d54265080b0f55
anhpham <anhhoang.pham@yum.com>
2023-06-19 11:57:21 +0700

    Test new models

--- script.js
+++ script.js
@@ -11,16 +11,25 @@ var models = [
         src: './assets/Pizza01/pizza.gltf',
         scale: '0.5 0.5 0.5',
         info: 'Cheese',
+        thumbnail: './assets/Pizza01/thumbnail.png'
     },
     {
         src: './assets/Pizza02/pizza.gltf',
         scale: '2 2 2',
         info: 'Salami',
+        thumbnail: './assets/Pizza02/thumbnail.png'
+    },
+    {
+        src: './assets/Pizza03/pizza.gltf',
+        scale: '1 1 1',
+        info: 'Pepperoni',
+        thumbnail: './assets/Pizza03/thumbnail.png'
     },
     {
         src: './assets/Pizza04/pizza.gltf',
         scale: '4.124475892003208 4.124475892003208 4.124475892003208',
         info: 'New pizza',
+        thumbnail: './assets/Pizza04/thumbnail.png'
     },
 
 ];
@@ -48,6 +57,13 @@ var setModel = function (model, entity) {
 function renderPizza() {
     let model = document.querySelector('a-entity');
     setModel(models[modelIndex], model);
+
+    let centeredDiv = document.querySelector('centered')
+    models.forEach((pizza) => {
+        let thumbnail = document.createElement('img')
+        thumbnail.setAttribute('src', pizza.thumbnail)
+        centeredDiv.appendChild(thumbnail)
+    })
 
     document.querySelector('button[data-action="change"]').addEventListener('click', function () {
         var entity = document.querySelector('a-entity');
